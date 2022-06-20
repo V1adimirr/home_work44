49002c710f8de9875385fea1af71460113a6622c
Vladimir <vladimirpedenko936@gmail.com>
2022-06-21 02:19:46 +0600

    "added file checker.py and 2 method 'check' and 'result_game'"

---
+++ home_work44/webapp/checker.py
@@ -0,0 +1,33 @@
+secret_num = ['1', '2', '3', '4']
+
+def check(numbers):
+    my_list = []
+    for i in numbers:
+        my_list.append(i)
+    numbers = my_list
+    print(int(numbers[0]))
+    if len(numbers) > 4 or len(numbers) < 4:
+        return "Enter only four numbers"
+    elif len(set(numbers)) < 4:
+        return "Values must not be repeated"
+    try:
+        for i in range(len(numbers)):
+            if int(numbers[i]) < 1 or int(numbers[i]) > 9:
+                return "Enter values from 1 to 9"
+    except ValueError:
+        return "Enter only numbers"
+
+def result_game(self):
+    count_bulls = 0
+    count_cows = 0
+    for i in range(len(self.user_numbers)):
+        if self.user_numbers[i] == self.secret_num[i]:
+            count_bulls += 1
+        elif self.user_numbers[i] in self.secret_num:
+            count_cows += 1
+    if count_bulls == 4:
+        return 'Your Win!'
+    elif count_bulls or count_cows:
+        return f'You got {count_bulls} bulls and {count_cows} cows'
+    else:
+        'No identical values'
