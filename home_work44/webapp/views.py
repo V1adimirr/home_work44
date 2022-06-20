b14ed165298ad47497094142185dfb530c0dbdea
Vladimir <vladimirpedenko936@gmail.com>
2022-06-21 02:20:49 +0600

    "save commit"

--- home_work44/webapp/views.py
+++ home_work44/webapp/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from webapp.checker import check
 
 def check_view(request):
-    pass
+    if request.method == "GET":
+        return render(request, "index.html")
+    else:
+        numbers = request.POST.get("numbers")
+        message = check(numbers)
+        print(message)
+
+
+        return render(request, "index.html")
